f12200ee76a94c091030438f19f196d279a746b9
Angus Hollands <goosey15@gmail.com>
2016-12-12 12:02:19 +0000

    Update __init__

---
+++ funnel/__init__.py
@@ -0,0 +1,3 @@
+"""Funnel is a simple language for writing schemas for Pythonic data models.
+It defines a few simple Block types, with a strict grammar, and allows particular blocks to embed raw Python code.
+"""
