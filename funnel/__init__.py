0fe0b909a00697cf478d7b86387af7b3f8f743be
Angus Hollands <goosey15@gmail.com>
2016-12-23 21:33:58 +0000

    Remove funnel example

--- funnel/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Funnel is a simple language for writing schemas for Pythonic data models.
-It defines a few simple Block types, with a strict grammar, and allows particular blocks to embed raw Python code.
-"""
