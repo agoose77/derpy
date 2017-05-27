e00e23f3adcc25a3fbdb6c5b76a5f7dcd88a06fd
Angus Hollands <goosey15@gmail.com>
2017-05-27 19:43:03 +0100

    Add docstring

--- derp/fields.py
+++ derp/fields.py
@@ -1,13 +1,18 @@
 _field_init_body = \
-"""
-def __init__(self, {arg_list}):
-    {assignment_body}
-"""
+    """
+    def __init__(self, {arg_list}):
+        {assignment_body}
+    """
 
 _field_assignment_stmt = "self.{0} = {0}"
 
 
 class FieldMeta(type):
+    """Metaclass which provides machinery to set named fields on class instance during initialisation.
+    
+    Use of this decorator automatically invokes the __slots__ mechanism
+    """
+
     def __new__(metacls, name, bases, cls_dict, fields=None):
         if fields is not None:
             field_names = tuple(fields.split())
