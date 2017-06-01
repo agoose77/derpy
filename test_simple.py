97101c17d2cc7179a38a2d0dc1250116f0eaa974
Angus Hollands <goosey15@gmail.com>
2017-06-01 15:15:25 +0100

    Update test

--- test_simple.py
+++ test_simple.py
@@ -6,10 +6,13 @@ if __name__ == '__main__' and 1:
     tokens = [Token('var', i) for i in range(5)]
 
     parse_greedy_example = +lit('var')
-    print(parse(parse_greedy_example, tokens))
+    greedy_parse_result = parse(parse_greedy_example, tokens)
+    print(greedy_parse_result)
 
     parse_one_plus_example = star(lit('var'))
     print(parse(parse_one_plus_example, tokens))
+
+    print(parse(parse_greedy_example, []))
 
     assert parse(parse_greedy_example, []) == {''}
     assert parse(parse_one_plus_example, []) == set()
