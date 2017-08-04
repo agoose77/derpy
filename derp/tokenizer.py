aca3cf5b0b977bac2e3eff265a95c899477853e8
Angus Hollands <goosey15@gmail.com>
2017-08-04 14:22:05 +0100

    More typing

--- derp/tokenizer.py
+++ derp/tokenizer.py
@@ -5,6 +5,11 @@ INT_REGEX = r"^[1-9][0-9]*$"
 
 from ast import literal_eval
 from re import compile as re_compile, escape
+from typing import Generator
+
+
+PatternType = type(re_compile('.'))
+MatchType = type(re_compile('.').match(' 'd))
 
 
 class Tokenizer:
@@ -27,18 +32,18 @@ class Tokenizer:
     def __init__(self):
         self.pattern = self.create_pattern()
 
-    def create_pattern(self):
+    def create_pattern(self) -> PatternType:
         patterns = self.patterns + (self.default_pattern,)
         full_match_string = "|".join(f'(?P<{n}>{m})' for n, m in patterns)
         return re_compile(full_match_string)
 
-    def create_context(self, string):
+    def create_context(self, string: str) -> dict:
         return {'line_number': 1, 'char_number': 0, 'string': string}
 
-    def default_handler(self, match, value, context):
+    def default_handler(self, match: MatchType, value, context: dict) -> Token:
         return Token(match.lastgroup, value)
 
-    def get_error_string(self, match, value, context):
+    def get_error_string(self, match: MatchType, value, context: dict) -> str:
         index = match.start() - context['char_number']
         lines = context['string'].splitlines()
         line = lines[context['line_number'] - 1]
@@ -46,7 +51,7 @@ class Tokenizer:
         indicator_string = ''.join('^' if i == index else ' ' for i, _ in enumerate(line))
         return f"Unable to match character {value!r} on line {context['line_number']}\n{line}\n{indicator_string}"
 
-    def tokenize_text(self, string):
+    def tokenize_text(self, string: str) -> Generator[Token, None, None]:
         context = self.create_context(string)
 
         for match in self.pattern.finditer(string):
@@ -63,16 +68,16 @@ class Tokenizer:
 
         yield Token("ENDMARKER", "ENDMARKER")
 
-    def tokenize_file(self, file_name):
+    def tokenize_file(self, file_name: str) -> Generator[Token, None, None]:
         with open(file_name) as f:
             yield from self.tokenize_text(f.read())
 
-    def handle_OP(self, match, value, context):
+    def handle_OP(self, match: MatchType, value, context: dict) -> Token:
         return Token(value, value)
 
     handle_PAREN = handle_OP
 
-    def handle_ID(self, match, value, context):
+    def handle_ID(self, match: MatchType, value, context: dict) -> Token:
         if value in self.keywords:
             kind = value
         else:
@@ -80,14 +85,14 @@ class Tokenizer:
 
         return Token(kind, value)
 
-    def handle_FORMAT(self, match, value, context):
+    def handle_FORMAT(self, match: MatchType, value, context: dict) -> None:
         pass
 
-    def handle_NEWLINE(self, match, value, context):
+    def handle_NEWLINE(self, match: MatchType, value, context: dict) -> Token:
         context['char_number'] = match.end()
         context['line_number'] += 1
 
         return Token("NEWLINE", value)
 
-    def handle_NUMBER(self, match, value, context):
+    def handle_NUMBER(self, match: MatchType, value, context: dict) -> Token:
         return Token("NUMBER", literal_eval(value))
