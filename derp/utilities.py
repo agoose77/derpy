697224b638a028906648f42bb601145fc4ad5bca
Angus Hollands <goosey15@gmail.com>
2017-08-04 13:35:38 +0100

    Remove text helpers

--- derp/utilities.py
+++ derp/utilities.py
@@ -1,42 +1,3 @@
-from collections import namedtuple
-from functools import wraps, partial as _partial
-
-TextContext = namedtuple("TextContext", "seen depth max_depth")
-
-
-def limited_depth_text(func):
-    """Prevent writing too much information to string than is legible"""
-
-    @wraps(func)
-    def wrapper(self, context):
-        if context.depth == context.max_depth:
-            return self.as_string
-
-        new_context = TextContext(context.seen, context.depth + 1, context.max_depth)
-        return func(self, new_context)
-
-    return wrapper
-
-
-def recursion_guard_text(func):
-    """Prevent recursive calls through to_text traversal"""
-
-    @wraps(func)
-    def wrapper(self, context):
-        if self not in context.seen:
-            context.seen.add(self)
-
-            return func(self, context)
-        return f"{self.as_string}(...)"
-
-    return wrapper
-
-
-def to_text_helper(func):
-    """Combinatined recursion guard and depth limiter"""
-    return recursion_guard_text(limited_depth_text(func))
-
-
 def flatten(seq, first=True):
     """Recursively flatten nested tuples into flat list
 
@@ -81,22 +42,28 @@ def unpack(seq, n, first=True):
 
 def extracts(n, *indices, first=True):
     """Reduction to extract given args from parser"""
+
     def wrapper(args):
         all_args = tuple(unpack(args, n, first))
         return tuple(all_args[i] for i in indices)
+
     return wrapper
 
 
 def extract(n, index, first=True):
     """Reduction to extract given arg from parser"""
+
     def wrapper(args):
         all_args = tuple(unpack(args, n, first))
         return all_args[index]
+
     return wrapper
 
 
 def partial(f, n, *indices, first=True):
     extractor = extracts(n, *indices, first)
+
     def wrapper(args):
         return f(*extractor(args))
+
     return wrapper
