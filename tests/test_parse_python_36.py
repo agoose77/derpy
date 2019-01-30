69c7c4ab3679f14a97bc9f52515fc86721a5eb18
Angus Hollands <goosey15@gmail.com>
2019-01-30 22:08:58 +0000

    Add round trip test

--- tests/test_parse_python_36.py
+++ tests/test_parse_python_36.py
@@ -1,6 +1,7 @@
+import os
 from unittest import TestCase, main
 
-from derpy import parse, Token
+from derpy import parse, Token, ast as derpy_ast
 from derpy.grammars.python36 import p, PythonTokenizer, ast
 
 test_string = "x = x + 1"
@@ -48,6 +49,31 @@ class TestParsePython(TestCase):
         module = next(iter(parse_trees))
 
         self.assertEqual(module, expected_ast)
+
+    def test_round_trip(self):
+        tokens = tuple(tokenizer.tokenize_text(test_string))
+        parse_trees = parse(p.file_input, tokens)
+
+        self.assertEqual(len(parse_trees), 1)
+        module = next(iter(parse_trees))
+
+        with ast.patched_ast_module():
+            import astor
+            try:
+                result = astor.to_source(module)
+            except AttributeError:
+                if derpy_ast.ENV_VAR_NO_SLOTS not in os.environ:
+                    raise EnvironmentError(f"Environment flag {derpy_ast.ENV_VAR_NO_SLOTS!r} must be defined to support "
+                                           "round-trip code generation.")
+                raise
+
+            namespace = {'x': 0}
+
+            exec(test_string, namespace)
+            self.assertEqual(namespace['x'], 1)
+
+            exec(result, namespace)
+            self.assertEqual(namespace['x'], 2)
 
 
 if __name__ == "__main__":
